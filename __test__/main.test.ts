be285bd60d2cb2af98f9f66c52a40bdc661b8210
erzetid <fahrizalm14@gmail.com>
2022-11-22 21:52:30 +0700

    feat: add enumCheck method test

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -8,6 +8,30 @@ const typeNumber: IMethod = (val: any) => {
   return { result: true, message: "the type is no number" };
 };
 
+enum enumExample{
+  NUMBER=0,
+  STRING='string'
+}
+const enumMessage = 'invalid type!';
+const enumSchema ={
+  number:CheckValue.enumCheck(enumExample,{message:enumMessage}),
+  string:CheckValue.enumCheck(enumExample,{message:enumMessage})
+}
+
+describe('Check enum', () => { 
+  it('Check is fail ❎ validation' , () => {
+    const _bad1 = new CheckValue().object(enumSchema).validate({number:false, string:'string'}, true)
+    const _bad2 = new CheckValue().object(enumSchema).validate({number:0, string:[]}, true)
+    expect(_bad1).toStrictEqual('invalid type!')
+    expect(_bad2).toStrictEqual('invalid type!')
+  });
+  
+  it('Check is pass ✅ validation' , () => {
+    const _good = new CheckValue().object(enumSchema).validate({number:0, string:'string'}, true)
+    expect(_good).toStrictEqual('')
+  });
+ })
+
 describe("Custom method", () => {
   it("is example for good value", () => {
     const _good = new CheckValue<{ price: string }>()
